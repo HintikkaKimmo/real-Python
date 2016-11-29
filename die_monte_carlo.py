6e5567ac4d588660ff3e8e38dfd6945ddce6cb05
HintikkaKimmo <hintikkakimmo@gmail.com>
2016-11-29 10:21:14 +0000

    fixed die Monte Carlo

--- die_monte_carlo.py
+++ die_monte_carlo.py
@@ -1,26 +1,10 @@
 from random import randint
 
-i = 0
-ii = 0
-iii = 0
-iv = 0
-v = 0
-vi = 0
+rolls = [0] * 6
 
 for trial in range(0, 10000):
     die = randint(1, 6)
-    if die == 1:
-        i += 1
-    elif die == 2:
-        ii += 1
-    elif die == 3:
-        iii += 1
-    elif die == 4:
-        iv += 1
-    elif die == 5:
-        v += 1
-    else:
-        vi += 1
+    rolls[die - 1] += 1
 
 print("i = {}, ii = {}, iii = {}, iv = {}, v = {}, vi = {}"
-      .format(i, ii, iii, iv, v, vi))
+      .format(* rolls))
